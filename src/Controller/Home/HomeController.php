6554a6e4c1efc801bf1b17c6eebd12ffad3757e9
Sonderman <alihaydar338@gmail.com>
2019-12-20 17:10:02 +0300

    23. video bitti. gmail ile mail gönderme yapıldı.

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -10,24 +10,28 @@ use App\Repository\SettingsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Bridge\Google\Smtp\GmailTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
- * @Route("/", name="home")
- */
-    public function index(SettingsRepository $settingsRepository,CarRepository $carRepository)
+     * @Route("/", name="home")
+     */
+    public function index(SettingsRepository $settingsRepository, CarRepository $carRepository)
     {
-        $data = $settingsRepository->findBy(['id'=>1]);
-        $slider = $carRepository->findBy([],['title'=>'ASC'],5);
+        $data = $settingsRepository->findBy(['id' => 1]);
+        $slider = $carRepository->findBy([], ['title' => 'ASC'], 5);
         //dump($slider);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'data'=>$data,
-            'slider'=>$slider
+            'data' => $data,
+            'slider' => $slider
         ]);
     }
+
     /**
      * @Route("/cars/{id}", name="car_show", methods={"GET"})
      */
@@ -37,40 +41,59 @@ class HomeController extends AbstractController
             'car' => $car,
         ]);
     }
+
     /**
      * @Route("/aboutus", name="aboutus")
      */
     public function aboutus(SettingsRepository $settingsRepository): Response
     {
-        $settings = $settingsRepository->findBy(['id'=>1]);
+        $settings = $settingsRepository->findBy(['id' => 1]);
         return $this->render('home/SinglePages/aboutus.html.twig', [
             'settings' => $settings,
         ]);
     }
+
     /**
      * @Route("/contact", name="contact", methods={"GET","POST"})
      */
-    public function contact(SettingsRepository $settingsRepository,Request $request): Response
+    public function contact(SettingsRepository $settingsRepository, Request $request): Response
     {
         $message = new Messages();
         $form = $this->createForm(MessagesType::class, $message);
         $form->handleRequest($request);
         $submittedToken = $request->request->get('token');
-
+        $settings = $settingsRepository->findBy(['id' => 1]);
         if ($form->isSubmitted()) {
 
-            if($this->isCsrfTokenValid('form-message',$submittedToken)){
-            $entityManager = $this->getDoctrine()->getManager();
-            $message->setStatus('New');
-            $message->setIp($_SERVER['REMOTE_ADDR']);
-            $entityManager->persist($message);
-            $entityManager->flush();
-            $this->addFlash('success','Your message has been sent succesfully');
-            return $this->redirectToRoute('contact');
+            if ($this->isCsrfTokenValid('form-message', $submittedToken)) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $message->setStatus('New');
+                $message->setIp($_SERVER['REMOTE_ADDR']);
+                $entityManager->persist($message);
+                $entityManager->flush();
+                $this->addFlash('success', 'Your message has been sent succesfully');
+
+
+                ///////////////////////Send Mail
+                $email = (new Email())
+                    ->from($settings[0]->getSmtpemail())
+                    ->to($form['email']->getData())
+                    ->subject('Thank you for message')
+                    ->html("Dear " . $form['name']->getData() . "<br>
+                        <p>We have received your message</p> Thank you<br>
+                            ============================================
+                        <br>" . $settings[0]->getCompany() . "<br> Address:" . $settings[0]->getAddress() . "<br> Phone:" . $settings[0]->getPhone() . "<br>"
+                    );
+                $transport = new GmailTransport($settings[0]->getSmtpemail(),$settings[0]->getSmtppassword());
+                $mailer = new  Mailer($transport);
+                $mailer->send($email);
+
+
+                return $this->redirectToRoute('contact');
             }
         }
 
-        $settings = $settingsRepository->findBy(['id'=>1]);
+
         return $this->render('home/SinglePages/contact.html.twig', [
             'settings' => $settings,
             'form' => $form->createView(),
